f8e52e9e1b382d3371922d90873bb9baef19d5dc
dhgarcia <danielj83wc@gmail.com>
2016-04-17 23:44:17 +0200

    Model
    2

--- PredictionModel.R
+++ PredictionModel.R
@@ -5,13 +5,27 @@ prediction <- function(inputText, Ngrams) {
   
   
   s<-1:3
-  lambdas <- c(0.45, 0.30, 0.20, 0.05)
-  words<-unlist(strsplit(inputText," "))
-  tokens <- sapply(s, function(i) tail(words,i))
+  lambdas <- c(0.05, 0.20, 0.30, 0.45)
+  tokens<-unlist(strsplit(inputText," "))
+  tokens <- sapply(s, function(i) tail(tokens,i))
   
+  for (i in length(Ngrams):1) {
+    tmp <- Ngrams[[i]]
+    tok <- paste(tokens[[i-1]],collapse = "_")
+    tok <- paste(tok, "_", sep = "")
+    #which(tmp$token == tok)
+    tmp <- tmp[grep(tok,tmp$token),]
+    #tmp <- mutate(tmp, prob = rcount/N)
+    #tmp <- arrange(tmp, desc(count))
+    tmp <- tmp %>% mutate(token = gsub(tok, "", token), prob = (rcount/N)*lambdas[i]) %>%
+      arrange(desc(prob)) %>% select(token, prob)
+    #tmp <- head(tmp)
+    predict <- tmp
+  }
   
-  
-  g<-tail(g,l)
-  g<-paste(g,collapse = "_")
-  
+  head(predict)
 }
+
+
+load("Corpus_NGrams_Frequencies_R.RData")
+prediction("Do you want to go to the", NGrams)
